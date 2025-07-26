e13dfe1fcbef8ddff811d5924e161619d60c8007
Shikhar Mall <shmall.21.2020@gmail.com>
2025-07-26 15:45:39 +0530

    header fixed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Banner({ title }) {
   return (
     <div
-      className="relative h-80 mb-12 flex items-center justify-center text-center bg-cover bg-center"
+      className="relative h-40 md:h-80 mb-12 flex items-center justify-center text-center bg-cover bg-center"
       style={{ backgroundImage: "url('/assets/img/royal4.jpg')" }}
     >
       <div className="absolute inset-0 bg-black opacity-50"></div>{" "}
